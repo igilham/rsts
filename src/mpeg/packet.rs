a8139dbdb94fd70ba581d292215883cd51a97b06
Ian Gilham <ian.gilham@gmail.com>
2019-07-02 09:43:05 +0100

    fix(mpeg): get tests working

--- src/mpeg/packet.rs
+++ src/mpeg/packet.rs
@@ -3,6 +3,7 @@ pub const HEADER_SIZE: usize = 4;
 pub const PAYLOAD_SIZE: usize = PACKET_SIZE - HEADER_SIZE;
 pub const SYNC_BYTE: u8 = 0x47;
 pub const MAX_PID: u16 = 0x1FFF;
+pub const NULL_PACKET_PID: u16 = MAX_PID;
 
 pub type Packet = [u8; PACKET_SIZE];
 
@@ -12,4 +13,31 @@ pub fn null_packet() -> Packet {
     packet[1] = 0x1f;
     packet[2] = 0xff;
     return packet;
+}
+
+pub fn set_transport_error(packet: &mut Packet) {
+    packet[1] = 0x80;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_null_packet() {
+        let packet: Packet = null_packet();
+        assert_eq!(packet[0], SYNC_BYTE);
+        assert_eq!(packet[1], 0x1f);
+        assert_eq!(packet[2], 0xff);
+        for i in 3..PACKET_SIZE {
+            assert_eq!(packet[i], 0xff);
+        }
+    }
+
+#[test]
+    fn test_set_transport_error() {
+        let mut packet = null_packet();
+        set_transport_error(&mut packet);
+        assert_eq!(packet[1], 0x80);
+    }
 }
